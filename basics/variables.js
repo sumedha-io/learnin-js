8907766bc061ddb413b05b60a1dd71d8a1429d7d
sumedha-io <sumedhap2007@gmail.com>
2026-07-20 09:36:43 +0000

    Basics of js

---
+++ basics/variables.js
@@ -0,0 +1,14 @@
+const accId = 14567367
+let accEmail = "su@gmail.com"
+var accPassword = "123848"
+accCity = "B'lore"
+let accState
+ 
+//accId =6 not allowed
+/*
+Prefer not to use var because
+of issue in block scope and functional scope 
+*/
+
+console.table([accId,accEmail,accPassword,accCity,accState])
+

0dc842fae7e1bab08c7248495e740c2dceac225b
sumedha-io <sumedhap2007@gmail.com>
2026-07-21 15:38:07 +0530

    Update 01variables.js

--- basics/01variables.js
+++ basics/01variables.js
@@ -11,4 +11,15 @@ of issue in block scope and functional scope
 */
 
 console.table([accId,accEmail,accPassword,accCity,accState])
-
+let i=1;
+if(true){
+ const a = 2;
+ let b= 3;
+ var c = 9;
+ let i=2
+ accPassword = "848"
+}
+//console.log(b) results in an error because of the block scope
+//console.log(a) results in an error because of the block scope
+console.log(c) //does not result in an error hence avoid var also  accPassword changes to "848" which should be "123848"
+console.log(i)//gives 1 not 2 because i will have 2 only in if block
